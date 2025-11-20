0e7c8027a044a62fd06ba104603f25e34f915550
ingo.steldermann <ingo.steldermann@rwth-aachen.de>
2025-11-20 09:55:53 +0100

    FEATURE: installation via conda + update dockers

--- install/set_conda_config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir -p $CONDA_PREFIX/etc/conda/activate.d
-
-echo 'export ZOOMY_DIR="${PWD}"' > $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
-echo 'export PYTHONPATH=":${PWD}/library"' >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
-echo 'export JAX_ENABLE_X64=True' >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
-
-
-
